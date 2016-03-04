276a4f4587141c779aec6f0aeb8ba18b548ecee8
probablycorey <probablycorey@github.com>
2016-03-03 16:55:10 -0800

    new compiled source

--- url-hunter.js
+++ url-hunter.js
@@ -2,10 +2,6 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _templateObject = _taggedTemplateLiteral(['#  You killed ', ' animal', ' in ', ' seconds! (Press ESC to play again)'], ['#  You killed ', ' animal', ' in ', ' seconds! (Press ESC to play again)']);
-
-function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Game = function () {
@@ -13,7 +9,6 @@ var Game = function () {
     _classCallCheck(this, Game);
 
     this.levelSize = 60;
-    console.log("hey");
     this.playerLocation = this.levelSize / 2;
   }
 
@@ -24,7 +19,7 @@ var Game = function () {
       this.startTime = new Date();
       this.timeLimit = 30; // seconds
       this.animals = [];
-      for (var i = 0; i++; i < 4) {
+      for (var i = 0; i < 4; i++) {
         var animal = new Animal(Math.floor(Math.random() * this.levelSize));
         this.animals.push(animal);
       }
@@ -34,8 +29,8 @@ var Game = function () {
   }, {
     key: 'gameOver',
     value: function gameOver() {
-      clearInterval(this.interval);
-      location.replace(_templateObject, this.points, this.points == 1 ? '' : '\'s', this.elapsedTime());
+      clearInterval(this.points);
+      location.replace('#  You killed ' + this.points + ' animal' + (this.points === 1 ? '' : '\'s') + ' in ' + this.elapsedTime() + ' seconds! (Press ESC to play again)');
     }
   }, {
     key: 'elapsedTime',
@@ -51,14 +46,14 @@ var Game = function () {
     key: 'removeAnimal',
     value: function removeAnimal(deadAnimal) {
       this.animals = this.animals.filter(function (animal) {
-        return animal != deadAnimal;
+        return animal !== deadAnimal;
       });
     }
   }, {
     key: 'animalAt',
     value: function animalAt(position) {
       return this.animals.find(function (animal) {
-        return Math.floor(animal.position) == position;
+        return Math.floor(animal.position) === position;
       });
     }
 
@@ -101,7 +96,7 @@ var Game = function () {
       var url = "";
       while (url.length < this.levelSize) {
         var position = url.length;
-        if (position == this.playerLocation) {
+        if (position === this.playerLocation) {
           url += this.animalAt(this.playerLocation) ? "@" : "O";
         } else if (this.animalAt(position)) {
           url += "a";
@@ -114,7 +109,9 @@ var Game = function () {
       if (timeLeft <= 0) {
         this.gameOver();
       } else {
-        if (timeLeft < 10) timeLeft = "0" + timeLeft; // Keep the same width
+        if (timeLeft < 10) {
+          timeLeft = "0" + timeLeft; // Keep the same width
+        }
         location.replace('#  ' + timeLeft + '|' + url + ('|' + timeLeft));
         document.title = 'Points ' + this.points;
       }
@@ -122,23 +119,27 @@ var Game = function () {
   }, {
     key: 'onKeyDown',
     value: function onKeyDown(event) {
-      if (event.which == 37) {
+      if (event.which === 37) {
         // left
         this.playerLocation -= 1;
-        if (this.playerLocation < 0) this.playerLocation = this.levelSize - 1;
-      } else if (event.which == 39) {
+        if (this.playerLocation < 0) {
+          this.playerLocation = this.levelSize - 1;
+        }
+      } else if (event.which === 39) {
         // right
         this.playerLocation += 1;
         this.playerLocation %= this.levelSize;
-      } else if (event.which == 38 || event.which == 32) {
+      } else if (event.which === 38 || event.which === 32) {
         // attack
         var animal = this.animalAt(this.playerLocation);
         if (animal) {
           this.points += 1;
           this.removeAnimal(animal);
-          if (this.animals.length == 0) this.gameOver();
+          if (this.animals.length === 0) {
+            this.gameOver();
+          }
         }
-      } else if (event.which == 27) {
+      } else if (event.which === 27) {
         // enter
         this.start();
       }
@@ -165,7 +166,9 @@ var Animal = function () {
       this.velocityIndex += Math.random() * this.velocityChange;
       this.position += Math.sin(this.velocityIndex) * dampener;
       this.position %= levelSize;
-      if (this.position < 0) this.position += levelSize;
+      if (this.position < 0) {
+        this.position += levelSize;
+      }
     }
   }]);
 
@@ -174,6 +177,8 @@ var Animal = function () {
 
 document.addEventListener("DOMContentLoaded", function () {
   var game = new Game();
-  document.addEventListener("keydown", game.onKeyDown);
+  document.addEventListener("keydown", function () {
+    return game.onKeyDown.apply(game, arguments);
+  });
   game.start();
 });
