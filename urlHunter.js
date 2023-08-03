8567d58b7e0bb02cfe2f422ec3f8df4b2dc9d1b6
Corey Johnson <probablycorey@gmail.com>
2023-08-03 15:24:38 -0700

    made some changes

--- urlHunter.js
+++ urlHunter.js
@@ -5,6 +5,7 @@ class Game {
   }
 
   start() {
+    this.lastFrame = Date.now()
     this.points = 0
     this.startTime = new Date()
     this.timeLimit = 30 // seconds
@@ -14,12 +15,16 @@ class Game {
       this.animals.push(animal)
     }
 
-    this.interval = setInterval(this.update.bind(this), 1000 / 30)
+    this.update()
   }
 
   gameOver() {
-    clearInterval(this.interval)
-    location.replace(`#  You killed ${this.points} animal${this.points === 1 ? '' : '\'s'} in ${this.elapsedTime()} seconds! (Press ESC to play again)`)
+    cancelAnimationFrame(this.cancelId)
+    location.replace(
+      `#__You_killed_${this.points}_animal${
+        this.points === 1 ? "" : "'s"
+      } in ${this.elapsedTime()}_seconds!_(Press_ESC_to_play_again)`
+    )
   }
 
   elapsedTime() {
@@ -34,29 +39,36 @@ class Game {
   }
 
   animalAt(position) {
-    return this.animals.find(animal => Math.floor(animal.position) === position)
+    return this.animals.find(
+      (animal) => Math.floor(animal.position) === position
+    )
   }
 
   // Gamestate Methods
- // -----------------
+  // -----------------
   update() {
-    for (let animal of this.animals) {
-      animal.update(this.levelSize)
+    const lastFrame = this.lastFrame
+    const delta = Date.now() - lastFrame
+    if (delta > 1000 / 10) {
+      this.lastFrame = Date.now()
+      for (let animal of this.animals) {
+        animal.update(this.levelSize)
+      }
+      this.draw()
     }
-    this.draw()
+
+    this.cancelId = window.requestAnimationFrame(() => this.update())
   }
 
   draw() {
     let url = ""
-    while(url.length < this.levelSize) {
+    while (url.length < this.levelSize) {
       let position = url.length
       if (position === this.playerLocation) {
         url += this.animalAt(this.playerLocation) ? "@" : "O"
-      }
-      else if (this.animalAt(position)) {
+      } else if (this.animalAt(position)) {
         url += "a"
-      }
-      else {
+      } else {
         url += "-"
       }
     }
@@ -64,28 +76,28 @@ class Game {
     let timeLeft = this.timeLimit - this.elapsedTime()
     if (timeLeft <= 0) {
       this.gameOver()
-    }
-    else {
+    } else {
       if (timeLeft < 10) {
         timeLeft = "0" + timeLeft // Keep the same width
       }
-      location.replace(`#  ${timeLeft}|` + url + `|${timeLeft}`)
+      location.replace(`#_seconds_left_${timeLeft}|` + url + `|${timeLeft}_`)
       document.title = `Points ${this.points}`
     }
   }
 
   onKeyDown(event) {
-    if (event.which === 37) { // left
+    if (event.which === 37) {
+      // left
       this.playerLocation -= 1
       if (this.playerLocation < 0) {
         this.playerLocation = this.levelSize - 1
       }
-    }
-    else if (event.which === 39) { // right
+    } else if (event.which === 39) {
+      // right
       this.playerLocation += 1
       this.playerLocation %= this.levelSize
-    }
-    else if (event.which === 38 || event.which === 32) { // attack
+    } else if (event.which === 38 || event.which === 32) {
+      // attack
       let animal = this.animalAt(this.playerLocation)
       if (animal) {
         this.points += 1
@@ -94,8 +106,8 @@ class Game {
           this.gameOver()
         }
       }
-    }
-    else if (event.which === 27) { // enter
+    } else if (event.which === 27) {
+      // enter
       this.start()
     }
   }
@@ -111,7 +123,7 @@ class Animal {
   update(levelSize) {
     let dampener = 0.4
 
-    this.velocityIndex += (Math.random() * this.velocityChange)
+    this.velocityIndex += Math.random() * this.velocityChange
     this.position += Math.sin(this.velocityIndex) * dampener
     this.position %= levelSize
     if (this.position < 0) {
@@ -121,12 +133,13 @@ class Animal {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  let isSafari = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1
+  let isSafari =
+    navigator.userAgent.indexOf("Safari") != -1 &&
+    navigator.userAgent.indexOf("Chrome") == -1
 
   if (isSafari) {
     document.body.classList.toggle("safari")
-  }
-  else {
+  } else {
     let game = new Game()
     document.addEventListener("keydown", (...args) => game.onKeyDown(...args))
     game.start()
